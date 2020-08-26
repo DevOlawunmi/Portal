0ee2f76f2aa2984c9bb24967022a9ef936e10f35
Ola <olawunmi263@yahoo.com>
2020-08-26 05:56:34 +0100

    Commit Initial Framework

---
+++ src/main/java/azure/net/hfp_recoveries/App.java
@@ -0,0 +1,13 @@
+package azure.net.hfp_recoveries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
