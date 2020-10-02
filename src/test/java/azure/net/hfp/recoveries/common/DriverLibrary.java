8caa0d226e1863e2dba22c9ba41caca3d439598d
Ola <olawunmi263@yahoo.com>
2020-10-02 04:20:37 +0100

    Driver class and Browser class added

---
+++ src/test/java/azure/net/hfp/recoveries/common/DriverLibrary.java
@@ -0,0 +1,9 @@
+package azure.net.hfp.recoveries.common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DriverLibrary {
+    public WebDriver driver;
+    public WebDriverWait wait;
+}
