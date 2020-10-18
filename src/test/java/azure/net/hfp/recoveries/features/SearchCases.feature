3f2594bd452276be198b8e582511ae297e6bf95d
Ola <olawunmi263@yahoo.com>
2020-10-18 05:14:21 +0100

    Search Cases Feature File added

---
+++ src/test/java/azure/net/hfp/recoveries/features/SearchCases.feature
@@ -0,0 +1,24 @@
+Feature: Search Cases
+  As a Recoveries Portal user
+  I want to search for my existing cases
+  So I can view their status
+  Background:
+    Given I navigate to Recoveries portal
+    And I enter my username
+    And I enter my password
+    And I click on the sign on button
+    And I click on LBA Instruction
+    Then  Search Cases page "<searchCasesPage>" is displayed
+
+  Scenario Outline: User Can Search Cases using our reference
+    And I enter our reference "<ourReference>"
+    And I click on search
+    Then existing cases are displayed
+
+    Examples:
+      | ourReference |
+    |189611/500    |
+
+
+
+
---
+++ src/test/java/azure/net/hfp/recoveries/stepDefinitions/SearchCasesSteps.java
@@ -0,0 +1,26 @@
+package azure.net.hfp.recoveries.stepDefinitions;
+
+import azure.net.hfp.recoveries.pages.BasePage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class SearchCasesSteps extends BasePage {
+
+    @Then("Search Cases page {string} is displayed")
+    public void searchCasesPageIsDisplayed(String arg0) {
+    }
+
+    @And("I enter our reference {string}")
+    public void iEnterOurReference(String arg0) {
+        
+    }
+
+    @And("I click on search")
+    public void iClickOnSearch() {
+        
+    }
+
+    @Then("existing cases are displayed")
+    public void existingCasesAreDisplayed() {
+    }
+}
