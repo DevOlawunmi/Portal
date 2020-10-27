f5cfc91d4f03120d71a2bda4def14ceb4a5aaae3
Ola <olawunmi263@yahoo.com>
2020-10-27 11:36:02 +0000

    Selenium notes added

--- src/test/java/azure/net/hfp/recoveries/common/DriverLibrary.java
+++ src/test/java/azure/net/hfp/recoveries/common/DriverLibrary.java
@@ -1,4 +1,19 @@
 package azure.net.hfp.recoveries.common;
+/*
+WebDriver is a tool for testing web applications across different browsers using different programming languages.
+WebDriver allows the use of programming language of choice in designing tests.
+It is not mandatory to write your Selenium code in the same language as the application.
+
+
+There are seven basic elements of a Selenium test script, which apply to any test case and any application under test (AUT):
+Create a WebDriver instance.
+Navigate to a Web page.
+Locate an HTML element on the Web page.
+Perform an action on an HTML element.
+Anticipate the browser response to the action.
+Run tests and record test results using a test framework.
+Conclude the test.
+ */
 
 
 import org.openqa.selenium.WebDriver;
